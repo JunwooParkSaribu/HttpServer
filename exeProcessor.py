a8ebd2a77eb2acfd238931c70729e528bf96cefa
JunwooParkSaribu <junwoo5071@gmail.com>
2023-07-12 15:27:05 +0200

    a

--- exeProcessor.py
+++ exeProcessor.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         print('Queue size: ', job_queue.qsize())
         print('Current running process:')
         for process in process_line:
-            print(process_line)
+            print(f'{process} : {process_line[process]}')
         print()
 
         """
