93d785203124106b89f29a5bbe4b76ae5260bb4f
JunwooParkSaribu <junwoo5071@gmail.com>
2023-10-31 11:01:16 +0100

    rad51 update

--- exeProcessor.py
+++ exeProcessor.py
@@ -6,19 +6,22 @@ import time
 import queue
 import os
 
+LINUX_PATH = '/home/junwoo'
+WINDOWS_SERVER_PATH = '/mnt/c/Users/jwoo/Desktop/HttpServer'
 DATA_FOLDER = 'C:/Users/jwoo/Desktop/HttpServer/data'
 SAVE_FOLDER = 'C:/Users/jwoo/Desktop/HttpServer/save'
-MODEL_PATH = 'C:/Users/jwoo/Desktop/HttpServer/model'
 DATA_BASE = 'C:/Users/jwoo/Desktop/HttpServer/fiona.db'
+
 SUBMITTIME_INDEX = -999
+NB_PROCESS = 1
 
 
-def configure_setting(data_path, save_path, model_path, job_id) -> bool:
+def configure_setting(data_path, save_path, job_id) -> bool:
     with open(f'{save_path}/{job_id}/config.txt', 'w') as f:
         input_str = ''
-        input_str += f'data = {data_path}/{job_id}\n'
-        input_str += f'save_dir = {save_path}/{job_id}\n'
-        input_str += f'model_dir = {model_path}/model_htc\n'
+        input_str += f'data = {WINDOWS_SERVER_PATH}/data/{job_id}\n'
+        input_str += f'save_dir = {WINDOWS_SERVER_PATH}/save/{job_id}\n'
+        input_str += f'model_dir = {LINUX_PATH}/HTC/model/histoneModel\n'
         input_str += f'cut_off = 8\n'
         input_str += f'all = False\n'
         input_str += f'makeImage = True\n'
@@ -45,7 +48,6 @@ def run_command(cmd):
 
 
 if __name__ == '__main__':
-    NB_PROCESS = 2
     job_id_index = -999
     job_type_index = -999
     sleep_time = 10
@@ -138,9 +140,10 @@ if __name__ == '__main__':
                 if next_job_type == 'H2B':
                     # ERR on mkdir is already checked on server script.
                     os.mkdir(f'{SAVE_FOLDER}/{next_job_id}')
-                    if configure_setting(data_path=DATA_FOLDER, save_path=SAVE_FOLDER,
-                                         model_path=MODEL_PATH, job_id=next_job_id):
-                        proc = run_command(['./h2b_pipe.exe', f'{SAVE_FOLDER}/{next_job_id}'])
+                    if configure_setting(data_path=DATA_FOLDER, save_path=SAVE_FOLDER, job_id=next_job_id):
+                        proc = run_command(
+                            ['wsl', 'python3', f'{LINUX_PATH}/HTC/h2b_prediction.py',
+                             f'/mnt/c/Users/jwoo/Desktop/HttpServer/save/{next_job_id}'])
 
                 elif next_job_type == 'Rad51':
                     # ERR on mkdir is already checked on server script.
@@ -150,8 +153,7 @@ if __name__ == '__main__':
                 elif next_job_type == 'Rad51_protein':
                     # ERR on mkdir is already checked on server script.
                     os.mkdir(f'{SAVE_FOLDER}/{next_job_id}')
-                    #proc = run_command(['./rad51_protein.exe', f'{next_job_id}'])
-                    proc = run_command(['wsl', 'python3', '/home/junwoo/nuclei_test/prediction.py', f'{next_job_id}'])
+                    proc = run_command(['wsl', 'python3', f'{LINUX_PATH}/nuclei_test/prediction.py', f'{next_job_id}'])
 
                 cursor.execute("UPDATE job SET status=? WHERE job_id=?;", ['running', next_job_id])
                 process_line[proc] = next_job_id
